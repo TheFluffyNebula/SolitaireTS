b5d9fddf7e3115f588e49d0028e5cd832e86ea33
Jason Yin <jyin23@ncsu.edu>
2025-04-15 14:17:46 -0400

    init card, next: html/css for playing field

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const AceOfSpades = {
+    suit: '♠',
+    value: 'A'
+  };
 
   return (
     <>
-      
+      <Card card={AceOfSpades}/>
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import '../css/Card.css';
+
+function Card({ card }) {
+  const { suit, value } = card;
+
+  return (
+    <div className="card">
+      <div className="corner top-left">
+        <div>{value}</div>
+        <div>{suit}</div>
+      </div>
+      <div className="center">
+        <div>{value} {suit}</div>
+      </div>
+      <div className="corner bottom-right">
+        <div>{value}</div>
+        <div>{suit}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
