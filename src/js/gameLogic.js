574a81afc1fe285264daf3e8835fb6ab00d7205e
Jason Yin <jyin23@ncsu.edu>
2025-04-22 14:13:21 -0400

    draw card logic (stock -> waste and back)

--- src/js/gameLogic.js
+++ src/js/gameLogic.js
@@ -1,1 +1,21 @@
 console.log("Hello World!");
+
+// stockPile implemented as a queue: idx = n - 1 is out first
+// wastePile implemented as a stack: idx = n - 1 is out first
+// a b c d e --> e d c b a
+// if we hit zero stock, make new stock, add back in reverse order
+
+export function drawCard(stockPile, wastePile) {
+    if (stockPile.length > 0) {
+        const newCard = stockPile[stockPile.length - 1];
+        return {
+          newStock: stockPile.slice(0, -1),
+          newWaste: [...wastePile, newCard],
+        };
+    } else {
+        return {
+            newStock: [...wastePile].reverse(), 
+            newWaste: []
+        }
+    }
+}
